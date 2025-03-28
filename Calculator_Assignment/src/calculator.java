d9d769de875a47a749aced05459515f3e6ca39ce
AlexandriaTH <118304167+AlexandriaTH@users.noreply.github.com>
2025-03-27 19:22:45 -0500

    Added Java Files

---
+++ Calculator_Assignment/src/calculator.java
@@ -0,0 +1,18 @@
+public class calculator {
+
+    public int addition(int x, int y) {
+        return x + y;
+    }
+
+    public int subtraction(int x, int y) {
+        return x - y;
+    }
+
+    public int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public int division(int x, int y) {
+        return x % y;
+    }
+}
---
+++ Calculator_Assignment/src/calculatorTest.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class calculatorTest {
+
+    @Test
+    public void add() {
+        calculator calc = new calculator();
+        calc.addition(21,1);
+        assertEquals(22, calc.addition(21,1));
+    }
+
+    @Test
+    public void subtract() {
+        calculator calc = new calculator();
+        calc.subtraction(21, 1);
+        assertEquals(20, calc.subtraction(21,1));
+    }
+
+    @Test
+    public void multiply() {
+        calculator calc = new calculator();
+        calc.multiplication(9,9);
+        assertEquals(81, calc.multiplication(9,9));
+    }
+
+    @Test
+    public void divide() {
+        calculator calc = new calculator();
+        calc.division(21,9);
+        assertEquals(3, calc.division(21,9));
+    }
+}
